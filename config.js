15508e955a63e67f12b659b5fb8889873049f41c
khunlingtan <kltan69@gmail.com>
2017-11-25 15:44:11 +0800

    passport faceboook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientId: '1691414440924642',
+    clientSecret: 'c6a261e38cdb9b704ec7c98e29ce41be'
+  }
 }
